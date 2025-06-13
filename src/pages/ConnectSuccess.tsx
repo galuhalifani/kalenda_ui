c2f542398488a7124b7c28582886ebd9ff5b3b42
Galuh Alifani <galuh.adika@gmail.com>
2025-06-13 17:06:22 +0700

    add routes

--- src/pages/ConnectSuccess.tsx
+++ src/pages/ConnectSuccess.tsx
@@ -1,7 +1,7 @@
 const ConnectSuccess = () => {
   return (
     <div style={{ padding: "2rem", fontSize: "1.2rem" }}>
-      ✅ Google Calendar connected! You can now use the bot.
+      ✅ Google Calendar connected! You can now add and fetch event from your own Google Calendar.
     </div>
   );
 };
